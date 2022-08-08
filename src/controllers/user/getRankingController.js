4ef6d075824cdf19908f5c157c5d36bd3587069e
Renan Araujo <ra.carvalho@unesp.br>
2022-08-08 02:54:03 -0300

    route get ranking

--- src/controllers/user/getRankingController.js
+++ src/controllers/user/getRankingController.js
@@ -1,5 +1,27 @@
-const getRankingController = async (req, res) =>{
+import { connection } from "../../database.js";
 
+const getRankingController = async (req, res) =>{
+try {
+    const {rows: ranking} = await connection.query(
+        `
+        SELECT 
+	        users.id,
+	        users.name, 
+            COUNT(urls.id) as "linksCount",
+	        SUM(urls."visitCount") as "visitCount"
+        FROM users
+        JOIN urls
+        ON urls."userId" = users.id
+        GROUP BY users.id
+        ORDER BY "visitCount" DESC
+        LIMIT 10;
+        `
+    );
+    res.status(200).send(ranking);
+} catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+}
 }
 
 export default getRankingController;
